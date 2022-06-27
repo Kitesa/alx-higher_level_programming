cb55e07289f4f06e97e8ba08fd0f49e26ed1b97a
Kitesa <kitesamerga@gmail.com>
2022-06-27 04:29:08 -0700

    Added a program that checks if there is a cycle or not

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,32 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has a cycle
- * @list: pointer to first element
- *
- * Return: 1 if cycle is found 0 else
+ * check_cycle - checks if a singly linked list have a cycle or not
+ * @list: singly linked lists
+ * Return: 1 if cycle 0 else
  */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *current, *next_node;
+	listint_t *head;
+	listint_t *tail;
 
 	if (list == NULL)
 		return (0);
 
-	current = list;
-	next_node = list;
+	head = list
+		tail = list;
 
-	while (current && next_node && next_node->next)
+	while (tail != NULL && tail->next != NULL)
 	{
-		current = current->next;
-		next_node = next_node->next->next;
-		if (current == new_node)
+		head = head->next;
+		tail = tail->next->next;
+
+		if (head == tail)
 			return (1);
+
 	}
-	if (current != next_node)
-		return (0);
-	return (1);
+
+	return (0);
 }
