6d21c47ec4afc9c2fe98e7a7213301cf33101672
Kitesa <kitesamerga@gmail.com>
2022-07-05 02:00:38 -0700

    Added a c function that checks if singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,28 +1,82 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: double pointer to list
- * Return: 1 if palindrome 0 else
+ * is_palindrome - checks if a singly linked list is palindrome
+ * @head: the head of singly linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 else
  */
 int is_palindrome(listint_t **head)
 {
-	return (checkPalindrome(head, *head));
+	listint_t *start = NULL, *end = NULL;
+	unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+
+	if (head == NULL)
+		return (0);
+
+	start = *head;
+	len = listint_len(start);
+	len_cyc = len * 2;
+	len_list = len_cyc - 2;
+	end = *head;
+
+	for (; i < len_cyc; i = i + 2)
+	{
+		if (start[i].n != end[len_list].n)
+			return (0);
+
+		len_list = len_list - 2;
+	}
+
+	return (1);
 }
 
 /**
- * checkPalindrome - recursive functionto check if singly linked list is pal
- * @headptr:double pointer to list
- * @tptr: pointer to the list
+ * get_nodeint_at_index - gets a node from a linked list
+ * @head: the head of the linkedlist
+ * @index: the index to find in the linkedd list
  *
- * Return: 1 or 0
+ * Return: the soecific node of the linked list
  */
-int checkPalindrome(listint_t **headptr, listint_t *tptr)
-{
-	int res;
 
-	if (tptr == NULL)
-		return (1);
-	res = checkPalindrome(headptr, tptr->next) && ((*headptr)->n == tptr->n);
-	return (res);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	unsigned int iter_times = 0;
+
+	if (head)
+	{
+		while (current != NULL)
+		{
+			if (iter_times == index)
+				return (current);
+
+			current = current->next;
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+ * slistint_len - counts the number ofd elements in the linked list
+ * @h: the linked list to count
+ *
+ * Return: Number of elements in the linked list
+ */
+
+size_t listint_len(const listint_t *n)
+{
+	int length = 0;
+
+	while (h != NULL)
+	{
+		++length;
+		h = h->next;
+	}
+
+	return (length);
 }
