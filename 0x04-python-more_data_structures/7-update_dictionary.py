3898b43200e2dc23cff49cfb78c840bf80f5dfe6
Kitesa <kitesamerga@gmail.com>
2022-07-06 05:48:45 -0700

    Added a function that replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,4 +4,6 @@ def update_dictionary(a_dictionary, key, value):
     for each_key in a_dictionary.keys():
         if each_key == key:
             a_dictionary[key] = value
+    if key not in a_dictionary.keys():
+        a_dictionary[key] = value
     return a_dictionary
