868e3e21d148aa59d56a7d3b44704907662913d4
Kitesa Merga <67538957+Kitesa@users.noreply.github.com>
2022-10-12 06:21:03 -0700

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 """
-Module that retrieves states searched by the user
+Lists all values in the states tables of a database where name
+matches the argument
 """
 import sys
 import MySQLdb
 
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-def connectToDB():
-    # Connecting to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    # Getting the state
-    statement = "SELECT * FROM states WHERE name = '{}'".format(sys.argv[4])
-    cur.execute(statement)
-    rows = cur.fetchall()
-    for item in rows:
-        print(item)
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
-
-if __name__ == "__main__":
-    connectToDB()
+    for state in states:
+        print(state)
