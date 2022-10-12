90bbe860aaeb0c55b5b661a895b225606084426f
Kitesa <kitesamerga@gmail.com>
2022-10-12 05:43:03 -0700

    Added a script that list all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,12 @@ from the database
 '''
 if __name__ == "__main__":
     con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
     cursor = con.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     db = cursor.fetchall()
