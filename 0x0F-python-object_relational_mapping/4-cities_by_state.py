38691ca6e17ca584d13397f2eff61bfb5736fb74
Kitesa <kitesamerga@gmail.com>
2022-10-12 06:56:34 -0700

    Added that lists all cities from database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,10 +12,8 @@ def connectToDB():
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name ,states.name \
+    cur.execute("SELECT * \
                  FROM cities \
-                 INNER JOIN states \
-                 ON cities.state_id = states.id \
                  ORDER BY cities.id")
     rows = cur.fetchall()
     for item in rows:
