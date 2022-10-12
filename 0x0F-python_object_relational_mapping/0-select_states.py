3fc45aa6c70b61e55cd84670db8c513bd2799213
Kitesa Merga <67538957+Kitesa@users.noreply.github.com>
2022-10-12 05:24:17 -0700

    added a script that displays all states

--- 0x0F-python_object_relational_mapping/0-select_states.py
+++ 0x0F-python_object_relational_mapping/0-select_states.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""
-List all states from mysql on local host at posrt 3306
-"""
-from mysqlman import MySQLMan
-from MySQLbd import Error
-from sys import argv, exit, stderr
+import MySQLdb
+from sys import argv
 
-HELP = '{} username password database'.format(argv[0])
-HOST = 'localhost'
-POSRT = 3306
-
-if __nama__ == '__main__':
-    try:
-        mysqlman = MySQLMan(
-            connect = True,
-            user = argv[1],
-            password = argv[2],
-            database = argv[3],
-            host = HOST,
-            posrt = PORT,
-            )
-    except IndexError:
-        stderr.write('usage: {}\n'.format(HELP))
-        exit(2)
-    except Error as e:
-        stderr.write('{}\n'.format.(e.args[1]))
-        exit(1)
-    query = "SELECT id, name FROM states ORDER BY id;"
-    results = mysqlman.query.([query, ()])
-    for row in results[0]:
-        print(row)
+'''
+a script that lists all states
+from the database
+'''
+if __name__ == "__main__":
+    con = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1],
+        password=argv[2], database=argv[3])
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    db = cursor.fetchall()
+    for i in db:
+        print(i)
+    cursor.close()
+    db.close()
