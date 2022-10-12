cac708260b9a16212b01719d2e488eb287dfb2a2
Kitesa <kitesamerga@gmail.com>
2022-10-12 06:51:32 -0700

    Added that lists all cities from database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A Module that lists all cities and their state from
+Module that lists all cities and their state from
 the database hbtn_0e_0_usa
 """
 import sys
@@ -10,7 +10,7 @@ import MySQLdb
 def connectToDB():
     # Connecting to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+                         password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name ,states.name \
                  FROM cities \
