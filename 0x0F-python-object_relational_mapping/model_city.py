a21fbbddb4c3240991f198dbbbf0018208a5e17e
Kitesa <kitesamerga@gmail.com>
2022-10-13 04:31:06 -0700

    Added model city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -15,6 +15,6 @@ class City(Base):
     '''
     __tablename__ = 'cities'
 
-    id = Column(Integer,. primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, Foreignkey('state.id'), nullable=False)
