49582accd7c37b0141fd4ba78bffb320b878fe26
Kitesa <kitesamerga@gmail.com>
2022-10-24 01:43:56 -0700

    ADded  a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that takes your Github credentials 
+Python script that takes your Github credentials
 (username and password)
 and uses the Github API to display your id
 """
